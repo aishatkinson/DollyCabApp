7030038c08980764b7c7363607f795705f84005e
Aishling Atkinson <aishling.atkinson@mycit.ie>
2018-11-14 11:15:27 +0000

    markers not working yet

--- app/src/main/java/com/example/user/dollypassengerapp/MapsActivity.java
+++ app/src/main/java/com/example/user/dollypassengerapp/MapsActivity.java
@@ -1,8 +1,19 @@
 package com.example.user.dollypassengerapp;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,24 +29,32 @@ import com.google.firebase.auth.FirebaseAuth;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private Button logout;
+    private LocationManager locMan;
+    private LocationListener locListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
 
-        logout = findViewById(R.id.logout);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+            }
+            else {
+                locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+                        locListener);
+            }
+
+
+
+        Button logout = findViewById(R.id.logout);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(MapsActivity.this,
-                           MapsActivity.class ));
+                          MainActivity.class ));
                 Toast.makeText(MapsActivity.this, "Signed Out!",
                         Toast.LENGTH_LONG).show();
 
@@ -43,6 +62,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] ==  PackageManager.PERMISSION_GRANTED){
+            if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                //Ask for Permission
+                locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+                        locListener);
+            }
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -57,10 +88,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        locMan = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("My location is: ", location.toString());
+
+                LatLng userLoc = new LatLng(52,12);
+                mMap.addMarker(new MarkerOptions().position(userLoc).title("Marker in Sydney"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(userLoc));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
     }
 
 
